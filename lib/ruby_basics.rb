2893506646e76c10962cf7b226e35524cc86d9a6
sbdensford <sbdensford@hotmail.com>
2016-03-29 09:46:10 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,36 @@
-def division(num1, num2)
- 
+def division(num1 = 42, num2 = 7)
+ puts "#{num1} / #{num2}"
+ 42 / 7 
 end
 
-def assign_variable(value)
+def assign_variable(value = "CC")
+  puts "#{value}"
+  "#{value}"
+end
+
+def argue(heated = "discussion")
+  puts "#{heated}"
+  "#{heated}"
+end
+
+def greeting(a, b)
+  puts "#{a}"
   
-end
-
-def argue
-  
-end
-
-def greeting
 
 end
 
-def return_a_value
+def return_a_value(phrase = "Nice")
+  puts "#{phrase}"
+  "#{phrase}"
 
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase = "expert")
+  puts "#{phrase}"
+  "#{phrase}"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  puts "#{topping}"
+  "#{topping}"
 end
